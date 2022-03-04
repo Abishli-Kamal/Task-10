bb6dde9240d906a426c321ae9d956649e5f95780
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-03-05 01:44:34 +0400

    task hazirdi

---
+++ task-2.1/task-2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int m = 0;
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("Sum is= " + sum);
+
+        }
+    }
+}
+
---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "Kamal", "Malik", "Tural", "Baxsheli", "Islam", "Bahadur" };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length>5)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
